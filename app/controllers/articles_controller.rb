ebe3875f2e4a67c5460bf2de37e95a0876a153ba
finnius25 <finneykoshy@gmail.com>
2022-04-09 16:25:13 -0500

    update & delete article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,8 +4,8 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])  
-  end 
+    @article = Article.find(params[:id])
+  end
 
   def new
     @article = Article.new
@@ -21,10 +21,29 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destory 
+    @article = Article.find(params[:id])
+    @article.destory
+
+    redirect_to :root_path, status: :see_other
+  end 
 
   private
     def article_params
       params.require(:article).permit(:title, :body)
-    end 
-
+    end
 end
